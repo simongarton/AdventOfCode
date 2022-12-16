b1de5f7873ab8df84bbb68d7f8715e8a9b969ab0
Simon Garton <simon.garton@gmail.com>
2022-12-16 18:36:28 +1300

    2022 Day 16.1 build network.

--- src/main/java/com/simongarton/adventofcode/year2022/Year2022Day16.java
+++ src/main/java/com/simongarton/adventofcode/year2022/Year2022Day16.java
@@ -2,7 +2,13 @@ package com.simongarton.adventofcode.year2022;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Year2022Day16 extends AdventOfCodeChallenge {
+
+    private List<Valve> valves;
+    private int pressureReleased;
 
     @Override
     public boolean run() {
@@ -11,11 +17,119 @@ public class Year2022Day16 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+        this.loadNetwork(input);
+        this.graphViz();
+        this.pressureReleased = 0;
+        this.getValve("BB").open();
+        for (int minute = 0; minute < 30; minute++) {
+            this.moveOpenOrStay();
+            this.pressureReleased += this.pressureFromOpenValves();
+        }
+        return String.valueOf(this.pressureReleased);
+    }
+
+    private void graphViz() {
+        System.out.println("digraph Volcano {");
+        for (final Valve valve : this.valves) {
+            for (final Valve tunnel : valve.tunnels) {
+                System.out.println(valve.name + " -> " + tunnel.name);
+            }
+        }
+        System.out.println("}");
     }
 
     @Override
     public String part2(final String[] input) {
         return null;
+    }
+
+
+    private void moveOpenOrStay() {
+
+    }
+
+    private int pressureFromOpenValves() {
+        return this.valves.stream().filter(v -> v.open).map(v -> v.releaseRate).mapToInt(Integer::intValue).sum();
+    }
+
+    private void loadNetwork(final String[] input) {
+        this.valves = new ArrayList<>();
+        this.loadValves(input);
+        this.digTunnels(input);
+    }
+
+    private void loadValves(final String[] input) {
+        for (final String line : input) {
+            this.loadValve(line);
+        }
+    }
+
+    private void loadValve(final String line) {
+        final String[] halves = line.split(";");
+        final String[] parts = halves[0].split(" ");
+        final String name = parts[1];
+        final String[] rateParts = parts[4].split("=");
+        final int rate = Integer.parseInt(rateParts[1]);
+        final Valve valve = new Valve(name, rate);
+        this.valves.add(valve);
+    }
+
+    private void digTunnels(final String[] input) {
+        for (final String line : input) {
+            this.digTunnel(line);
+        }
+    }
+
+    private void digTunnel(final String line) {
+        final String[] halves = line.split(";");
+        final String[] parts = halves[0].split(" ");
+        final String name = parts[1];
+        final Valve from = this.getValve(name);
+
+        final String search = halves[1].contains("valves") ? "valves " : "valve ";
+
+        final String[] tunnelParts = halves[1].split(search);
+        final String[] tunnels = tunnelParts[1].split(" ");
+        for (final String tunnel : tunnels) {
+            final String other = tunnel.replace(",", "");
+            final Valve to = this.getValve(other);
+            from.tunnel(to);
+        }
+    }
+
+    private Valve getValve(final String name) {
+        return this.valves.stream()
+                .filter(v -> v.name.equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Not found"));
+    }
+
+    public static final class Valve {
+
+        private final String name;
+        private final int releaseRate;
+        private boolean open;
+        private final List<Valve> tunnels;
+
+        public Valve(final String name, final int releaseRate) {
+            this.name = name;
+            this.releaseRate = releaseRate;
+            this.open = false;
+            this.tunnels = new ArrayList<>();
+        }
+
+        public Valve tunnel(final Valve other) {
+            final boolean exists = this.tunnels.contains(other);
+            if (exists) {
+                return this;
+            }
+            this.tunnels.add(other);
+            other.tunnels.add(this);
+            return this;
+        }
+
+        public void open() {
+            this.open = true;
+        }
     }
 }
--- src/test/java/com/simongarton/adventofcode/year2022/Year2022DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2022/Year2022DayTest.java
@@ -18,13 +18,13 @@ class Year2022DayTest {
     void part1() throws IOException {
 
         // given
-        final Path path = Paths.get("src/main/resources/2022/sample3.txt");
-//        final Path path = Paths.get("src/main/resources/2022/2022-Day13-1.txt");
+        final Path path = Paths.get("src/main/resources/2022/sample4.txt");
+//        final Path path = Paths.get("src/main/resources/2022/2022-Day16-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2022Day13 year2022Day13 = new Year2022Day13();
+        final Year2022Day16 year2022Day16 = new Year2022Day16();
 
         // when
-        final String result = year2022Day13.part1(lines.toArray(new String[0]));
+        final String result = year2022Day16.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -36,12 +36,12 @@ class Year2022DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2022/sample.txt");
-        final Path path = Paths.get("src/main/resources/2022/2022-Day13-2.txt");
+        final Path path = Paths.get("src/main/resources/2022/2022-Day16-2.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2022Day13 year2022Day13 = new Year2022Day13();
+        final Year2022Day16 year2022Day16 = new Year2022Day16();
 
         // when
-        final String result = year2022Day13.part2(lines.toArray(new String[0]));
+        final String result = year2022Day16.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
