a3dba7c37c8ae1ff9acb56690c431c6553913ce5
Simon Garton <simon.garton@gmail.com>
2022-12-22 05:30:48 +1300

    2022, Day 21.1 in done.

--- src/main/java/com/simongarton/adventofcode/year2022/Year2022Day21.java
+++ src/main/java/com/simongarton/adventofcode/year2022/Year2022Day21.java
@@ -2,7 +2,19 @@ package com.simongarton.adventofcode.year2022;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Year2022Day21 extends AdventOfCodeChallenge {
+
+    private static final boolean DEBUG = false;
+
+    @Override
+    public String title() {
+        return "Day 21: Monkey Math";
+    }
 
     @Override
     public boolean run() {
@@ -11,11 +23,114 @@ public class Year2022Day21 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+
+        final List<Monkey> troop = this.loadMonkeys(input);
+        troop.stream().forEach(m -> m.addDependencies(troop));
+        final Monkey root = troop.stream().filter(m -> m.name.equalsIgnoreCase("root"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Didn't find root"));
+        int iteration = 0;
+        while (!root.hasYelled) {
+            this.debugPrint(String.format("\niteration %s\n\n", iteration));
+            troop.stream().forEach(Monkey::lookAroundAndYell);
+            iteration++;
+        }
+        this.debugPrint("\n");
+        return String.valueOf(root.numberYelled);
     }
 
     @Override
     public String part2(final String[] input) {
         return null;
+    }
+
+    private void debugPrint(final String s) {
+        if (DEBUG) {
+            System.out.printf(s);
+        }
+    }
+
+    private List<Monkey> loadMonkeys(final String[] input) {
+        return Arrays.stream(input).map(Monkey::new).collect(Collectors.toList());
+    }
+
+    public static final class Monkey {
+
+        String name;
+        boolean hasYelled = false;
+        long numberYelled;
+        String calculation;
+        String operation;
+        List<Monkey> dependencies;
+
+        public Monkey(final String line) {
+            final String[] parts = line.split(":");
+            this.name = parts[0].trim();
+            this.calculation = parts[1].trim();
+        }
+
+        public int addDependencies(final List<Monkey> troop) {
+            this.dependencies = new ArrayList<>();
+            if (this.calculation.contains(" ")) {
+                final String[] parts = this.calculation.split(" ");
+                this.operation = parts[1];
+                this.dependencies.add(troop.stream()
+                        .filter(m -> m.name.equalsIgnoreCase(parts[0]))
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("Didn't find " + parts[0])));
+                this.dependencies.add(troop.stream()
+                        .filter(m -> m.name.equalsIgnoreCase(parts[2]))
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("Didn't find " + parts[2])));
+            } else {
+                this.numberYelled = Long.parseLong(this.calculation);
+                return 0;
+            }
+            if (this.dependencies.size() != 2) {
+                throw new RuntimeException("Bad dependencies " + this.dependencies.size() + " for " + this.calculation);
+            }
+            return this.dependencies.size();
+        }
+
+        public void lookAroundAndYell() {
+            if (this.hasYelled) {
+                return;
+            }
+            if (this.numberYelled > 0) {
+                this.debugPrint(String.format("%s yells out %s\n", this.name, this.numberYelled));
+                this.hasYelled = true;
+                return;
+            }
+            if (!(this.dependencies.get(0).hasYelled && this.dependencies.get(1).hasYelled)) {
+                return;
+            }
+            final long value1 = this.dependencies.get(0).numberYelled;
+            final long value2 = this.dependencies.get(1).numberYelled;
+            switch (this.operation) {
+                case "/":
+                    this.numberYelled = value1 / value2;
+                    break;
+                case "+":
+                    this.numberYelled = value1 + value2;
+                    break;
+                case "-":
+                    this.numberYelled = value1 - value2;
+                    break;
+                case "*":
+                    this.numberYelled = value1 * value2;
+                    break;
+                default:
+                    throw new RuntimeException("Bad operation " + this.operation);
+            }
+            this.debugPrint(String.format("%s yells out %s\n", this.name, this.numberYelled));
+            this.hasYelled = true;
+        }
+
+        private void debugPrint(final String s) {
+            if (DEBUG) {
+                System.out.printf(s);
+            }
+        }
+
     }
 }
--- src/test/java/com/simongarton/adventofcode/year2022/Year2022DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2022/Year2022DayTest.java
@@ -19,12 +19,12 @@ class Year2022DayTest {
 
         // given
         final Path path = Paths.get("src/main/resources/2022/sample.txt");
-//        final Path path = Paths.get("src/main/resources/2022/2022-Day19-1.txt");
+//        final Path path = Paths.get("src/main/resources/2022/2022-Day21-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2022Day19 year2022Day19 = new Year2022Day19();
+        final Year2022Day21 year2022Day21 = new Year2022Day21();
 
         // when
-        final String result = year2022Day19.part1(lines.toArray(new String[0]));
+        final String result = year2022Day21.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -36,12 +36,12 @@ class Year2022DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2022/sample.txt");
-        final Path path = Paths.get("src/main/resources/2022/2022-Day19-2.txt");
+        final Path path = Paths.get("src/main/resources/2022/2022-Day21-2.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2022Day19 year2022Day19 = new Year2022Day19();
+        final Year2022Day21 year2022Day21 = new Year2022Day21();
 
         // when
-        final String result = year2022Day19.part2(lines.toArray(new String[0]));
+        final String result = year2022Day21.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
