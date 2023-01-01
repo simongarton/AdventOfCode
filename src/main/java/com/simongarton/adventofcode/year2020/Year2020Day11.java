3b1cd455cfb8aa46a31fc9b4918149e3cb31973b
Simon Garton <simon.garton@gmail.com>
2023-01-01 20:13:49 +1300

    2020, Day 11.

--- src/main/java/com/simongarton/adventofcode/year2020/Year2020Day11.java
+++ src/main/java/com/simongarton/adventofcode/year2020/Year2020Day11.java
@@ -2,7 +2,16 @@ package com.simongarton.adventofcode.year2020;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Year2020Day11 extends AdventOfCodeChallenge {
+
+    @Override
+    public String title() {
+        return "Day 11: Seating System";
+    }
 
     @Override
     public Outcome run() {
@@ -11,11 +20,162 @@ public class Year2020Day11 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+        final List<String> lines = Arrays.asList(input);
+        return String.valueOf(this.runToSolution(lines, 1));
     }
 
     @Override
     public String part2(final String[] input) {
-        return null;
+        final List<String> lines = Arrays.asList(input);
+        return String.valueOf(this.runToSolution(lines, 2));
+    }
+
+
+    private int runToSolution(final List<String> lines, final int part) {
+        int iterations = 0;
+        List<String> working = new ArrayList<>(lines);
+        //print(0, working);
+        while (true) {
+            List<String> updated = new ArrayList<>(working);
+            updated = part == 1 ? this.update1(updated) : this.update2(updated);
+            iterations++;
+            if (this.noChange(working, updated)) {
+                break;
+            }
+            working = new ArrayList<>(updated);
+            //print(iterations, working);
+        }
+        return this.countOccupied(working);
+    }
+
+    private int countOccupied(final List<String> current) {
+        int occupied = 0;
+        for (final String s : current) {
+            for (int col = 0; col < s.length(); col++) {
+                if (s.substring(col, col + 1).equalsIgnoreCase("#")) {
+                    occupied++;
+                }
+            }
+        }
+        return occupied;
+    }
+
+    private void print(final int iterations, final List<String> working) {
+        System.out.println("iteration " + iterations + "\n");
+        working.forEach(System.out::println);
+        System.out.println("");
+    }
+
+    private List<String> update1(final List<String> current) {
+        final List<String> updated = new ArrayList<>();
+        for (int row = 0; row < current.size(); row++) {
+            final StringBuilder newRow = new StringBuilder();
+            for (int col = 0; col < current.get(row).length(); col++) {
+                final String currentSeat = current.get(row).substring(col, col + 1);
+                String replaceSeat = ".";
+                final int adjacent = this.countAdjacent(current, row, col);
+                if (currentSeat.equalsIgnoreCase("L")) {
+                    replaceSeat = adjacent == 0 ? "#" : "L";
+                }
+                if (currentSeat.equalsIgnoreCase("#")) {
+                    replaceSeat = adjacent >= 4 ? "L" : "#";
+                }
+                newRow.append(replaceSeat);
+            }
+            updated.add(newRow.toString());
+        }
+        return updated;
+    }
+
+    private List<String> update2(final List<String> current) {
+        final List<String> updated = new ArrayList<>();
+        for (int row = 0; row < current.size(); row++) {
+            final StringBuilder newRow = new StringBuilder();
+            for (int col = 0; col < current.get(row).length(); col++) {
+                final String currentSeat = current.get(row).substring(col, col + 1);
+                String replaceSeat = ".";
+                final int adjacent = this.countVisible(current, row, col);
+                if (currentSeat.equalsIgnoreCase("L")) {
+                    replaceSeat = adjacent == 0 ? "#" : "L";
+                }
+                if (currentSeat.equalsIgnoreCase("#")) {
+                    replaceSeat = adjacent >= 5 ? "L" : "#";
+                }
+                newRow.append(replaceSeat);
+            }
+            updated.add(newRow.toString());
+        }
+        return updated;
+    }
+
+    private int countAdjacent(final List<String> current, final int row, final int col) {
+        int neighbours = 0;
+        neighbours += this.countNeighbour(current, row + 1, col + 1);
+        neighbours += this.countNeighbour(current, row + 1, col + 0);
+        neighbours += this.countNeighbour(current, row + 1, col - 1);
+        neighbours += this.countNeighbour(current, row + 0, col + 1);
+        neighbours += this.countNeighbour(current, row + 0, col - 1);
+        neighbours += this.countNeighbour(current, row - 1, col + 1);
+        neighbours += this.countNeighbour(current, row - 1, col + 0);
+        neighbours += this.countNeighbour(current, row - 1, col - 1);
+        return neighbours;
+    }
+
+    private int countVisible(final List<String> current, final int row, final int col) {
+        int neighbours = 0;
+        neighbours += this.countVisibleNeighbour(current, row, col, 1, 1);
+        neighbours += this.countVisibleNeighbour(current, row, col, 1, 0);
+        neighbours += this.countVisibleNeighbour(current, row, col, 1, -1);
+        neighbours += this.countVisibleNeighbour(current, row, col, 0, 1);
+        neighbours += this.countVisibleNeighbour(current, row, col, 0, -1);
+        neighbours += this.countVisibleNeighbour(current, row, col, -1, 1);
+        neighbours += this.countVisibleNeighbour(current, row, col, -1, 0);
+        neighbours += this.countVisibleNeighbour(current, row, col, -1, -1);
+        return neighbours;
+    }
+
+    private int countVisibleNeighbour(final List<String> current, int row, int col, final int rowDelta, final int colDelta) {
+        while (true) {
+            row = row + rowDelta;
+            col = col + colDelta;
+            if (row < 0 || row >= current.size()) {
+                return 0;
+            }
+            final String line = current.get(row);
+            if (col < 0 || col >= line.length()) {
+                return 0;
+            }
+            final String seat = line.substring(col, col + 1);
+            if (seat.equalsIgnoreCase(".")) {
+                continue;
+            }
+            return seat.equalsIgnoreCase("#") ? 1 : 0;
+        }
+    }
+
+    private int countNeighbour(final List<String> current, final int row, final int col) {
+        if (row < 0 || row >= current.size()) {
+            return 0;
+        }
+        final String line = current.get(row);
+        if (col < 0 || col >= line.length()) {
+            return 0;
+        }
+        return line.substring(col, col + 1).equalsIgnoreCase("#") ? 1 : 0;
+    }
+
+    private boolean noChange(final List<String> working, final List<String> updated) {
+        if (working.size() != updated.size()) {
+            throw new RuntimeException("Wrong number of rows");
+        }
+        for (int i = 0; i < working.size(); i++) {
+            if (working.get(i).length() != updated.get(i).length()) {
+                throw new RuntimeException("Wrong number of cols at line " + i);
+            }
+            if (!working.get(i).equalsIgnoreCase(updated.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
@@ -19,12 +19,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day10-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day11-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day10 year2020Day10 = new Year2020Day10();
+        final Year2020Day11 year2020Day11 = new Year2020Day11();
 
         // when
-        final String result = year2020Day10.part1(lines.toArray(new String[0]));
+        final String result = year2020Day11.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -36,12 +36,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day10-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day11-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day10 year2020Day10 = new Year2020Day10();
+        final Year2020Day11 year2020Day11 = new Year2020Day11();
 
         // when
-        final String result = year2020Day10.part2(lines.toArray(new String[0]));
+        final String result = year2020Day11.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
