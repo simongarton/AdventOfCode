8b50998e9e56af68ac732e417e8d90b80a5b00f6
Simon Garton <simon.garton@gmail.com>
2023-01-01 19:40:01 +1300

    2020, Day 3 input and answers updated.

--- src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
@@ -19,12 +19,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day5-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day3-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day5 year2020Day5 = new Year2020Day5();
+        final Year2020Day3 year2020Day3 = new Year2020Day3();
 
         // when
-        final String result = year2020Day5.part1(lines.toArray(new String[0]));
+        final String result = year2020Day3.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -36,12 +36,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day5-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day3-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day5 year2020Day5 = new Year2020Day5();
+        final Year2020Day3 year2020Day3 = new Year2020Day3();
 
         // when
-        final String result = year2020Day5.part2(lines.toArray(new String[0]));
+        final String result = year2020Day3.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
