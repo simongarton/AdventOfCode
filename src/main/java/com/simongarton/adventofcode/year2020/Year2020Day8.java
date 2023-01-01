cdc3bf4c7fbafecfe61deb6924c8f1bebaec8d93
Simon Garton <simon.garton@gmail.com>
2023-01-01 19:56:52 +1300

    2020, Day 8.

--- src/main/java/com/simongarton/adventofcode/year2020/Year2020Day8.java
+++ src/main/java/com/simongarton/adventofcode/year2020/Year2020Day8.java
@@ -1,8 +1,21 @@
 package com.simongarton.adventofcode.year2020;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.*;
+
+import static com.simongarton.adventofcode.year2020.Year2020Day8.InstructionType.*;
 
 public class Year2020Day8 extends AdventOfCodeChallenge {
+
+    private Computer computer;
+
+    @Override
+    public String title() {
+        return "Day 8: Handheld Halting";
+    }
 
     @Override
     public Outcome run() {
@@ -11,11 +24,128 @@ public class Year2020Day8 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+        final List<String> lines = Arrays.asList(input);
+        this.buildComputer(lines);
+        this.computer.run();
+        return String.valueOf(this.computer.accumulator);
     }
 
     @Override
     public String part2(final String[] input) {
-        return null;
+        final List<String> lines = Arrays.asList(input);
+        this.buildComputer(lines);
+        return String.valueOf(this.computer.findBadInstruction(lines));
+    }
+
+
+    private void buildComputer(final List<String> lines) {
+        this.computer = new Computer();
+        this.computer.loadProgram(lines);
+    }
+
+    public enum InstructionType {
+        NOP,
+        ACC,
+        JMP
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Instruction {
+
+        private InstructionType type;
+        private final int value;
+    }
+
+    public static class Computer {
+
+        private final List<Instruction> instructions;
+        private int pointer;
+        private int accumulator;
+        private final Set<Integer> visited;
+
+        public Computer() {
+            this.instructions = new ArrayList<>();
+            this.visited = new HashSet<>();
+        }
+
+        public boolean run() {
+            while (true) {
+                if (this.pointer >= this.instructions.size()) {
+                    //System.out.printf("finished with p %d a %d\n", pointer, accumulator);
+                    return true;
+                }
+                final Instruction instruction = this.instructions.get(this.pointer);
+                //System.out.printf("p %d a %d i %s v %d\n", pointer, accumulator, instruction.type, instruction.value);
+                if (this.visited.contains(this.pointer)) {
+                    //System.out.printf("loop with p %d a %d\n", pointer, accumulator);
+                    return false;
+                }
+                this.visited.add(this.pointer);
+                switch (instruction.type) {
+                    case NOP:
+                        this.pointer++;
+                        break;
+                    case ACC:
+                        this.accumulator += instruction.value;
+                        this.pointer++;
+                        break;
+                    case JMP:
+                        this.pointer += instruction.value;
+                        break;
+                }
+            }
+        }
+
+        public Integer findBadInstruction(final List<String> lines) {
+            final Integer result = this.test(lines, NOP, JMP);
+            if (result == null) {
+                return this.test(lines, JMP, NOP);
+            } else {
+                return result;
+            }
+        }
+
+        private Integer test(final List<String> lines, final InstructionType from, final InstructionType to) {
+            for (int i = 0; i < lines.size(); i++) {
+                this.loadProgram(lines);
+                final Instruction instruction = this.instructions.get(i);
+                if (instruction.type != from) {
+                    continue;
+                }
+                instruction.type = to;
+                final boolean outcome = this.run();
+                if (outcome) {
+                    return this.accumulator;
+                }
+            }
+            return null;
+        }
+
+        public void loadProgram(final List<String> lines) {
+            this.instructions.clear();
+            this.visited.clear();
+            lines.forEach(this::processLine);
+            this.pointer = 0;
+            this.accumulator = 0;
+        }
+
+        private void processLine(final String line) {
+            final String[] parts = line.split(" ");
+            final int value = Integer.parseInt(parts[1]);
+            switch (parts[0]) {
+                case "nop":
+                    this.instructions.add(new Instruction(NOP, value));
+                    break;
+                case "acc":
+                    this.instructions.add(new Instruction(ACC, value));
+                    break;
+                case "jmp":
+                    this.instructions.add(new Instruction(JMP, value));
+                    break;
+                default:
+                    throw new RuntimeException("unrecognised instruction " + parts[0]);
+            }
+        }
     }
 }
--- src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2020/Year2020DayTest.java
@@ -19,12 +19,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day7-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day8-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day7 year2020Day7 = new Year2020Day7();
+        final Year2020Day8 year2020Day8 = new Year2020Day8();
 
         // when
-        final String result = year2020Day7.part1(lines.toArray(new String[0]));
+        final String result = year2020Day8.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -36,12 +36,12 @@ class Year2020DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2020/sample.txt");
-        final Path path = Paths.get("src/main/resources/2020/2020-Day7-1.txt");
+        final Path path = Paths.get("src/main/resources/2020/2020-Day8-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2020Day7 year2020Day7 = new Year2020Day7();
+        final Year2020Day8 year2020Day8 = new Year2020Day8();
 
         // when
-        final String result = year2020Day7.part2(lines.toArray(new String[0]));
+        final String result = year2020Day8.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
