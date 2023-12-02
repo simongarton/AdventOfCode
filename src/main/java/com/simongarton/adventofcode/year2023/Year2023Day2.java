5c49ed03d22a2b2c683e19417fbc2f4dd8b487e0
Simon Garton <simon.garton@gmail.com>
2023-12-02 18:19:13 +1300

    Day 2 part 1.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day2.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day2.java
@@ -1,6 +1,11 @@
 package com.simongarton.adventofcode.year2023;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Year2023Day2 extends AdventOfCodeChallenge {
 
@@ -16,11 +21,87 @@ public class Year2023Day2 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+        final Hand testHand = new Hand(12, 13, 14);
+        long score = 0;
+        for (final String line : input) {
+            final Game game = this.readGame(line);
+            this.playGame(game, testHand);
+            if (game.isPossible()) {
+                score += game.getId();
+            }
+        }
+        return String.valueOf(score);
+    }
+
+    private void playGame(final Game game, final Hand testHand) {
+        boolean possible = true;
+        for (final Hand hand : game.getHands()) {
+            if (hand.getRed() > testHand.getRed()) {
+                possible = false;
+                break;
+            }
+            if (hand.getGreen() > testHand.getGreen()) {
+                possible = false;
+                break;
+            }
+            if (hand.getBlue() > testHand.getBlue()) {
+                possible = false;
+                break;
+            }
+        }
+        game.setPossible(possible);
+    }
+
+    private Game readGame(final String line) {
+        final String[] mainParts = line.split(":");
+        final long id = Long.valueOf(mainParts[0].replace("Game ", ""));
+        final String[] handLines = mainParts[1].split(";");
+        final List<Hand> hands = new ArrayList<>();
+        for (final String handLine : handLines) {
+            final int red = this.countCubes("red", handLine);
+            final int green = this.countCubes("green", handLine);
+            final int blue = this.countCubes("blue", handLine);
+            final Hand hand = new Hand(red, green, blue);
+            hands.add(hand);
+        }
+        final Game game = new Game();
+        game.setId(id);
+        game.setHands(hands);
+        return game;
+    }
+
+    private int countCubes(final String color, final String line) {
+        final String[] cubeParts = line.split(",");
+        int cubes = 0;
+        for (final String cube : cubeParts) {
+            final String[] bits = cube.trim().split(" ");
+            if (bits[1].equalsIgnoreCase(color)) {
+                cubes += Integer.parseInt(bits[0]);
+            }
+        }
+        return cubes;
     }
 
     @Override
     public String part2(final String[] input) {
         return null;
+    }
+
+
+    @Data
+    private static class Game {
+
+        private long id;
+        List<Hand> hands;
+        private boolean possible;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Hand {
+
+        private int red;
+        private int green;
+        private int blue;
     }
 }
--- src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
@@ -18,8 +18,8 @@ class Year2023DayTest {
     void part1() throws IOException {
 
         // given
-        final Path path = Paths.get("src/main/resources/2023/sample.txt");
-//        final Path path = Paths.get("src/main/resources/2023/2023-Day2-1.txt");
+//        final Path path = Paths.get("src/main/resources/2023/sample.txt");
+        final Path path = Paths.get("src/main/resources/2023/2023-Day2-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
         final Year2023Day2 year2023Day2 = new Year2023Day2();
 
