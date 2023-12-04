f6ba94c4af1186e208353198a7d7b32d7c974ad0
Simon Garton <simon.garton@gmail.com>
2023-12-04 18:36:58 +1300

    Day 4 part 2 working.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day4.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day4.java
@@ -16,7 +16,7 @@ public class Year2023Day4 extends AdventOfCodeChallenge {
 
     @Override
     public String title() {
-        return "Day 3: Gear Ratios";
+        return "Day 4: Scratchcards";
     }
 
     @Override
@@ -49,6 +49,16 @@ public class Year2023Day4 extends AdventOfCodeChallenge {
         return matching;
     }
 
+    private int countWinners(final Card card) {
+        int matching = 0;
+        for (final Integer myNumber : card.getMyNumbers()) {
+            if (card.getWinningNumbers().contains(myNumber)) {
+                matching++;
+            }
+        }
+        return matching;
+    }
+
     private List<Card> readCards(final String[] input) {
         final List<Card> cards = new ArrayList<>();
         for (final String line : input) {
@@ -59,13 +69,16 @@ public class Year2023Day4 extends AdventOfCodeChallenge {
 
     private Card readCard(final String line) {
         final String[] parts = line.split(":");
+        final String name = parts[0];
+        final int id = Integer.valueOf(name.replace("Card ", "").replace(" ", " ").trim());
         final String[] numberParts = parts[1].trim().split("\\|");
         final String winningNumbersString = this.cleanString(numberParts[0].trim());
         final String myNumbersString = this.cleanString(numberParts[1].trim());
         final List<Integer> winningNumbers = Stream.of(winningNumbersString.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
         final List<Integer> myNumbers = Stream.of(myNumbersString.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
         return Card.builder()
-                .name(parts[0])
+                .id(id)
+                .name(name)
                 .winningNumbers(winningNumbers)
                 .myNumbers(myNumbers)
                 .build();
@@ -82,7 +95,28 @@ public class Year2023Day4 extends AdventOfCodeChallenge {
     @Override
     public String part2(final String[] input) {
 
-        return null;
+        final List<Card> cards = this.readCards(input);
+        final List<Card> originalCards = new ArrayList<>(cards);
+        final List<Card> newCards = new ArrayList<>();
+        int index = 0;
+
+        while (true) {
+            newCards.clear();
+            for (int i = index; i < cards.size(); i++) {
+                final Card card = cards.get(i);
+                final int winners = this.countWinners(card);
+                for (int j = 0; j < winners; j++) {
+                    newCards.add(originalCards.get(card.getId() + j));
+                }
+            }
+            if (index == cards.size()) {
+                break;
+            }
+            index = cards.size();
+            cards.addAll(newCards);
+        }
+
+        return String.valueOf(cards.size());
     }
 
     @Data
@@ -90,6 +124,7 @@ public class Year2023Day4 extends AdventOfCodeChallenge {
     @NoArgsConstructor
     @Builder
     private static final class Card {
+        private int id;
         private String name;
         private List<Integer> winningNumbers;
         private List<Integer> myNumbers;
