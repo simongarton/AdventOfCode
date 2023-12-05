8aa38f2a4460b4aab2066e6b941f4bb1ffbbd600
Simon Garton <simon.garton@gmail.com>
2023-12-05 19:10:44 +1300

    part 2, caching, Java heap space.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day5.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day5.java
@@ -4,15 +4,15 @@ import com.simongarton.adventofcode.AdventOfCodeChallenge;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Year2023Day5 extends AdventOfCodeChallenge {
 
-    private List<Long> seeds;
+    private List<Long> seeds = new ArrayList<>();
+    private List<SeedRange> seedRanges = new ArrayList<>();
     private List<AlmanacMap> maps;
-
+    private final Map<String, Map<Long, Long>> cache = new HashMap<>();
+    private final Map<Long, Long> locationCache = new HashMap<>();
 
     @Override
     public String title() {
@@ -27,6 +27,7 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
     @Override
     public String part1(final String[] input) {
 
+        this.loadSeeds(input);
         this.loadData(input);
 
         long lowest = Long.MAX_VALUE;
@@ -40,25 +41,64 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
         return String.valueOf(lowest);
     }
 
+    private long location(final SeedRange seedRange) {
+        long lowest = Long.MAX_VALUE;
+        for (long seed = seedRange.start; seed < seedRange.start + seedRange.length; seed++) {
+            final long location = this.location(seed);
+            if (location < lowest) {
+                lowest = location;
+            }
+        }
+        System.out.println("for seedRange " + seedRange + " I got " + lowest);
+        return lowest;
+    }
+
     private long location(final Long seed) {
+        if (this.locationCache.containsKey(seed)) {
+            return this.locationCache.get(seed);
+        }
         final long soil = this.map(seed, "seed-to-soil");
-        System.out.println(" seed " + seed + " maps to soil " + soil);
+//        System.out.println(" seed " + seed + " maps to soil " + soil);
         final long fertilizer = this.map(soil, "soil-to-fertilizer");
-        System.out.println("  soil " + soil + " maps to fertilizer " + fertilizer);
+//        System.out.println("  soil " + soil + " maps to fertilizer " + fertilizer);
         final long water = this.map(fertilizer, "fertilizer-to-water");
-        System.out.println("   fertilizer " + fertilizer + " maps to water " + water);
+//        System.out.println("   fertilizer " + fertilizer + " maps to water " + water);
         final long light = this.map(water, "water-to-light");
-        System.out.println("    water " + water + " maps to light " + light);
+//        System.out.println("    water " + water + " maps to light " + light);
         final long temperature = this.map(light, "light-to-temperature");
-        System.out.println("     light " + light + " maps to temperature " + temperature);
+//        System.out.println("     light " + light + " maps to temperature " + temperature);
         final long humidity = this.map(temperature, "temperature-to-humidity");
-        System.out.println("      temperature " + humidity + " maps to humidity " + humidity);
+//        System.out.println("      temperature " + humidity + " maps to humidity " + humidity);
         final long location = this.map(humidity, "humidity-to-location");
-        System.out.println("       seed " + seed + " maps to location " + location);
+//        System.out.println("       seed " + seed + " maps to location " + location);
+        this.locationCache.put(seed, location);
         return location;
     }
 
     private long map(final Long lookup, final String mapName) {
+        if (!this.cache.containsKey(mapName)) {
+            this.cache.put(mapName, new HashMap<>());
+        }
+        if (this.cache.get(mapName).containsKey(lookup)) {
+            return this.cache.get(mapName).get(lookup);
+        }
+        final AlmanacMap almanacMap = this.maps.stream().filter(m -> m.getName().equalsIgnoreCase(mapName))
+                .findFirst().orElseThrow(() -> new RuntimeException(mapName));
+        for (final AlmanacRange almanacRange : almanacMap.getRanges()) {
+            final long sourceStart = almanacRange.getSourceStart();
+            final long sourceEnd = sourceStart + almanacRange.getRange();
+            if (lookup < sourceStart || lookup > sourceEnd) {
+                continue;
+            }
+            final long value = lookup - sourceStart + almanacRange.getDestinationStart();
+            this.cache.get(mapName).put(lookup, value);
+            return value;
+        }
+//        this.cache.get(mapName).put(lookup, lookup);
+        return lookup;
+    }
+
+    private long mapBruteForce(final Long lookup, final String mapName) {
         final AlmanacMap almanacMap = this.maps.stream().filter(m -> m.getName().equalsIgnoreCase(mapName))
                 .findFirst().orElseThrow(() -> new RuntimeException(mapName));
         for (final AlmanacRange almanacRange : almanacMap.getRanges()) {
@@ -73,9 +113,17 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
         return lookup;
     }
 
-    private void loadData(final String[] input) {
+    private void loadSeeds(final String[] input) {
         final String firstLine = input[0];
         this.seeds = this.readSeeds(firstLine);
+    }
+
+    private void loadSeedRanges(final String[] input) {
+        final String firstLine = input[0];
+        this.seedRanges = this.readSeedRanges(firstLine);
+    }
+
+    private void loadData(final String[] input) {
 
         this.maps = new ArrayList<>();
         String mapName = null;
@@ -112,7 +160,8 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
             ranges.add(almanacRange);
         }
 
-        System.out.println("Read " + this.maps.size() + " maps for " + this.seeds.size() + " seeds.");
+        System.out.println("Read " + this.maps.size() + " maps for " + this.seeds.size() + " seeds and " +
+                this.seedRanges.size() + " seed ranges.");
     }
 
     private List<Long> readSeeds(final String firstLine) {
@@ -125,10 +174,39 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
         return seeds;
     }
 
+    private List<SeedRange> readSeedRanges(final String firstLine) {
+        final String[] parts = firstLine.split(":");
+        final String[] seedNumbers = parts[1].trim().split(" ");
+        final List<Long> seeds = new ArrayList<>();
+        final List<SeedRange> seedRanges = new ArrayList<>();
+        for (final String seedNumber : seedNumbers) {
+            seeds.add(Long.parseLong(seedNumber));
+            if (seeds.size() == 2) {
+                seedRanges.add(SeedRange.builder()
+                        .start(seeds.get(0))
+                        .length(seeds.get(1))
+                        .build());
+                seeds.clear();
+            }
+        }
+        return seedRanges;
+    }
+
     @Override
     public String part2(final String[] input) {
 
-        return null;
+        this.loadSeedRanges(input);
+        this.loadData(input);
+
+        long lowest = Long.MAX_VALUE;
+        for (final SeedRange seedRange : this.seedRanges) {
+            final long lowestLocation = this.location(seedRange);
+            if (lowestLocation < lowest) {
+                lowest = lowestLocation;
+            }
+        }
+
+        return String.valueOf(lowest);
     }
 
     @Data
@@ -136,6 +214,13 @@ public class Year2023Day5 extends AdventOfCodeChallenge {
     private static final class AlmanacMap {
         private String name;
         private List<AlmanacRange> ranges;
+    }
+
+    @Data
+    @Builder
+    private static final class SeedRange {
+        private long start;
+        private long length;
     }
 
     @Data
