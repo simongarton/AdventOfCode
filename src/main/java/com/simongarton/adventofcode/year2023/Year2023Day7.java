bc36643c963ab70e5bccd1355c221a58f16799d3
Simon Garton <simon.garton@gmail.com>
2023-12-07 21:51:20 +1300

    2023 Day 7 complete.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day7.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day7.java
@@ -25,7 +25,7 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
     public String part1(final String[] input) {
 
         this.handAndBidList = this.readCards(input);
-        this.handAndBidList.sort(new HandAndBitComparator());
+        this.handAndBidList.sort(new HandAndBitComparator(true));
         long total = 0;
         for (int i = 0; i < this.handAndBidList.size(); i++) {
             total = total + ((long) this.handAndBidList.get(i).getBid() * (i + 1));
@@ -37,19 +37,28 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
         final List<HandAndBid> handAndBidList = new ArrayList<>();
         for (final String hand : input) {
             final String[] parts = hand.split(" ");
-            final HandAndBid handAndBid = HandAndBid.builder()
+            handAndBidList.add(HandAndBid.builder()
                     .cards(parts[0])
+                    .originalCards(parts[0])
                     .bid(Integer.parseInt(parts[1]))
-                    .build();
-            handAndBid.setHand(handAndBid.calculateHand());
-            handAndBidList.add(handAndBid);
+                    .build()
+                    .calculateHand());
         }
         return handAndBidList;
     }
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        this.handAndBidList = this.readCards(input);
+        this.handAndBidList.sort(new HandAndBitWildComparator());
+        long total = 0;
+        for (int i = 0; i < this.handAndBidList.size(); i++) {
+            final HandAndBid handAndBid = this.handAndBidList.get(i);
+            handAndBid.calculateHand();
+            total = total + ((long) this.handAndBidList.get(i).getBid() * (i + 1));
+        }
+        return String.valueOf(total);
     }
 
     @Data
@@ -57,40 +66,37 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
     private static final class HandAndBid {
 
         private String cards;
+        private String originalCards;
         private int bid;
         private Hand hand;
 
-        public Hand calculateHand() {
+        public HandAndBid calculateHand() {
             if (this.fiveOfAKind(this.cards)) {
-                return Hand.FIVE_OF_A_KIND;
+                this.hand = Hand.FIVE_OF_A_KIND;
+                return this;
             }
             if (this.fourOfAKind(this.cards)) {
-                return Hand.FOUR_OF_A_KIND;
+                this.hand = Hand.FOUR_OF_A_KIND;
+                return this;
             }
             if (this.fullHouse(this.cards)) {
-                return Hand.FULL_HOUSE;
+                this.hand = Hand.FULL_HOUSE;
+                return this;
             }
             if (this.threeOfAKind(this.cards)) {
-                return Hand.THREE_OF_A_KIND;
+                this.hand = Hand.THREE_OF_A_KIND;
+                return this;
             }
             if (this.twoPair(this.cards)) {
-                return Hand.TWO_PAIR;
+                this.hand = Hand.TWO_PAIR;
+                return this;
             }
             if (this.onePair(this.cards)) {
-                return Hand.ONE_PAIR;
+                this.hand = Hand.ONE_PAIR;
+                return this;
             }
-            return Hand.HIGH_CARD;
-        }
-
-        private boolean onePair(final String cards) {
-            int pairs = 0;
-            final Map<String, Integer> counts = this.countCards(cards);
-            for (final Map.Entry<String, Integer> count : counts.entrySet()) {
-                if (count.getValue() == 2) {
-                    pairs++;
-                }
-            }
-            return pairs == 1;
+            this.hand = Hand.HIGH_CARD;
+            return this;
         }
 
         private boolean fullHouse(final String cards) {
@@ -108,7 +114,15 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
             return got2 && got3;
         }
 
+        private boolean onePair(final String cards) {
+            return this.countPairs(cards) == 1;
+        }
+
         private boolean twoPair(final String cards) {
+            return this.countPairs(cards) == 2;
+        }
+
+        private int countPairs(final String cards) {
             int pairs = 0;
             final Map<String, Integer> counts = this.countCards(cards);
             for (final Map.Entry<String, Integer> count : counts.entrySet()) {
@@ -116,7 +130,7 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
                     pairs++;
                 }
             }
-            return pairs == 2;
+            return pairs;
         }
 
         private boolean threeOfAKind(final String cards) {
@@ -151,6 +165,28 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
                 counts.put(c, counts.getOrDefault(c, 0) + 1);
             }
             return counts;
+        }
+
+        public int betterThanOriginal(final HandAndBid otherHand) {
+            for (int i = 0; i < 5; i++) {
+                int me = Rank.fromFace(this.originalCards.substring(i, i + 1)).getValue();
+                if (this.originalCards.substring(i, i + 1).equalsIgnoreCase(Rank.JACK.getFace())) {
+                    me = 1;
+                }
+                int them = Rank.fromFace(otherHand.originalCards.substring(i, i + 1)).getValue();
+                if (otherHand.originalCards.substring(i, i + 1).equalsIgnoreCase(Rank.JACK.getFace())) {
+                    them = 1;
+                }
+                if (me > them) {
+                    return 1;
+                }
+                if (me < them) {
+                    return -1;
+                }
+            }
+            System.out.println(this);
+            System.out.println(otherHand);
+            throw new RuntimeException("No ranking");
         }
 
         public int betterThan(final HandAndBid otherHand) {
@@ -219,19 +255,103 @@ public class Year2023Day7 extends AdventOfCodeChallenge {
             }
             throw new RuntimeException("Not found : " + face);
         }
+
+        public static Rank fromValue(final int value) {
+            for (final Rank rank : Rank.values()) {
+                if (rank.getValue() == value) {
+                    return rank;
+                }
+            }
+            throw new RuntimeException("Not found : " + value);
+        }
     }
 
-    private class HandAndBitComparator implements Comparator<HandAndBid> {
+    private static class HandAndBitComparator implements Comparator<HandAndBid> {
+
+        private final boolean useOriginalCards;
+
+        HandAndBitComparator(final boolean useOriginalCards) {
+            this.useOriginalCards = useOriginalCards;
+        }
 
         @Override
         public int compare(final HandAndBid firstHand, final HandAndBid secondHand) {
             if (firstHand.getHand().getValue() == secondHand.getHand().getValue()) {
-                return firstHand.betterThan(secondHand);
+                if (this.useOriginalCards) {
+                    return firstHand.betterThanOriginal(secondHand);
+                } else {
+                    return firstHand.betterThan(secondHand);
+                }
             } else {
                 return Integer.compare(firstHand.getHand().getValue(), secondHand.getHand().getValue());
             }
+        }
+    }
 
+    private static class HandAndBitWildComparator implements Comparator<HandAndBid> {
+
+        @Override
+        public int compare(final HandAndBid firstHand, final HandAndBid secondHand) {
+            final HandAndBid bestFirstHand = this.figureOutBestHand(firstHand);
+            final HandAndBid bestSecondHand = this.figureOutBestHand(secondHand);
+            final HandAndBitComparator handAndBitComparator = new HandAndBitComparator(true);
+            return handAndBitComparator.compare(bestFirstHand, bestSecondHand);
         }
 
+        private HandAndBid figureOutBestHand(final HandAndBid hand) {
+            if (hand.getCards().equalsIgnoreCase("JJJJJ")) {
+                return HandAndBid.builder()
+                        .cards("AAAAA")
+                        .originalCards(hand.getCards())
+                        .bid(hand.getBid())
+                        .build()
+                        .calculateHand();
+            }
+            final List<HandAndBid> allPossibleHands = new ArrayList<>();
+            for (int a = this.minValue(hand.cards.substring(0, 1)); a <= this.maxValue(hand.cards.substring(0, 1)); a++) {
+                for (int b = this.minValue(hand.cards.substring(1, 2)); b <= this.maxValue(hand.cards.substring(1, 2)); b++) {
+                    for (int c = this.minValue(hand.cards.substring(2, 3)); c <= this.maxValue(hand.cards.substring(2, 3)); c++) {
+                        for (int d = this.minValue(hand.cards.substring(3, 4)); d <= this.maxValue(hand.cards.substring(3, 4)); d++) {
+                            for (int e = this.minValue(hand.cards.substring(4, 5)); e <= this.maxValue(hand.cards.substring(4, 5)); e++) {
+                                final String newHand = this.getCard(a) +
+                                        this.getCard(b) +
+                                        this.getCard(c) +
+                                        this.getCard(d) +
+                                        this.getCard(e);
+                                final HandAndBid handAndBid = HandAndBid.builder()
+                                        .cards(newHand)
+                                        .originalCards(hand.getCards())
+                                        .bid(hand.getBid())
+                                        .build()
+                                        .calculateHand();
+                                if (!allPossibleHands.contains(handAndBid)) {
+                                    allPossibleHands.add(handAndBid);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            allPossibleHands.sort(new HandAndBitComparator(false).reversed());
+            return allPossibleHands.get(0);
+        }
+
+        private String getCard(final int value) {
+            return Rank.fromValue(value).getFace();
+        }
+
+        private int maxValue(final String card) {
+            if (card.equals(Rank.JACK.getFace())) {
+                return Rank.ACE.getValue();
+            }
+            return Rank.fromFace(card).getValue();
+        }
+
+        private int minValue(final String card) {
+            if (card.equals(Rank.JACK.getFace())) {
+                return Rank.TWO.getValue();
+            }
+            return Rank.fromFace(card).getValue();
+        }
     }
 }
