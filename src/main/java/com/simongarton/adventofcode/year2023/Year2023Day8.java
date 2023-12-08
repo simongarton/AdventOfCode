742eef3befd046c6d2ec7c961f4865fc4dc76bfd
Simon Garton <simon.garton@gmail.com>
2023-12-08 19:57:05 +1300

    Brute force is not working.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day8.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day8.java
@@ -4,7 +4,9 @@ import com.simongarton.adventofcode.AdventOfCodeChallenge;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Year2023Day8 extends AdventOfCodeChallenge {
@@ -12,6 +14,7 @@ public class Year2023Day8 extends AdventOfCodeChallenge {
     private Node firstNode;
     private Node lastNode;
     private Node currentNode;
+    private List<Node> currentNodes;
     private final Map<String, Node> nodeMap = new HashMap<>();
     private String directions;
     private int index;
@@ -75,7 +78,76 @@ public class Year2023Day8 extends AdventOfCodeChallenge {
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        this.loadMap(input);
+        long steps = 0;
+
+        this.currentNodes = new ArrayList<>();
+        for (final Map.Entry<String, Node> node : this.nodeMap.entrySet()) {
+            if (node.getKey().substring(2, 3).equalsIgnoreCase("A")) {
+                this.currentNodes.add(this.nodeMap.get(node.getKey()));
+            }
+        }
+
+        System.out.println(steps + " " + this.currentNodes.size());
+        this.debugNodes(steps);
+        while (!this.finished()) {
+            this.moveMany();
+            steps++;
+//            System.out.println(steps + " " + this.currentNodes.size());
+            this.debugNodes(steps);
+        }
+
+        return String.valueOf(steps);
+    }
+
+    private void debugNodes(final long steps) {
+        String line = "";
+        for (final Node node : this.currentNodes) {
+            line = line + node.getId() + ",";
+        }
+        if (steps % 1000000 == 0) {
+            System.out.println(steps + ":" + line.substring(0, line.length() - 1));
+        }
+    }
+
+    private boolean finished() {
+        for (final Node node : this.currentNodes) {
+            if (!node.getId().substring(2, 3).equalsIgnoreCase("Z")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void moveMany() {
+
+        final String direction = this.directions.substring(this.index, this.index + 1);
+        this.index = this.index + 1;
+        if (this.index == this.directions.length()) {
+            this.index = 0;
+        }
+
+        final List<Node> originals = new ArrayList<>();
+        final List<Node> splits = new ArrayList<>();
+        for (Node node : this.currentNodes) {
+            final boolean both = (node.getId().substring(2, 3).equalsIgnoreCase("A"));
+            if (direction.equalsIgnoreCase("L")) {
+                node = this.nodeMap.get(node.getLeft());
+                if (both) {
+                    splits.add(this.nodeMap.get(node.getRight()));
+                }
+            } else {
+                node = this.nodeMap.get(node.getRight());
+                if (both) {
+                    splits.add(this.nodeMap.get(node.getLeft()));
+                }
+            }
+            originals.add(node);
+        }
+        this.currentNodes.clear();
+        this.currentNodes.addAll(originals);
+//        this.currentNodes.addAll(splits);
     }
 
     @Data
