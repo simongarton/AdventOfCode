7b3c6276e0090f84112e44dc13bfde5bc28d67c9
Simon Garton <simon.garton@gmail.com>
2023-12-09 18:36:58 +1300

    Day 9 part 2 done.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day9.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day9.java
@@ -65,6 +65,29 @@ public class Year2023Day9 extends AdventOfCodeChallenge {
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        long total = 0;
+        for (final String line : input) {
+            total += this.solvePreviousValue(line);
+        }
+
+        return String.valueOf(total);
     }
+
+    private long solvePreviousValue(final String line) {
+        this.listOfLists.clear();
+        this.listOfLists.add(this.convertLineToList(line));
+        while (this.nonZeroList(this.listOfLists.get(this.listOfLists.size() - 1))) {
+            this.listOfLists.add(this.listOfDifferences(this.listOfLists.get(this.listOfLists.size() - 1)));
+        }
+        // now work backwards
+        for (int i = this.listOfLists.size() - 1; i > 0; i--) {
+            final List<Integer> topList = this.listOfLists.get(i - 1);
+            final List<Integer> bottomList = this.listOfLists.get(i);
+            topList.add(0, topList.get(0) - bottomList.get(0));
+            // do something with the difference in this line which will inform the difference in the previous one.
+        }
+        return this.listOfLists.get(0).get(0);
+    }
+
 }
