e68f97a6ae1a1b98b07ef6a9203ea76b239fa146
Simon Garton <simon.garton@gmail.com>
2023-12-10 20:39:11 +1300

    Part 2 breaking with walls that change direction.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day10.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day10.java
@@ -17,6 +17,23 @@ public class Year2023Day10 extends AdventOfCodeChallenge {
     For all connected neighbours, mark as N + 1;
     For neighbours, I have to check if they ARE neighbours
 
+    Part 2 :
+    loop over rows, cols, so coming in from left
+    keep track of if I'm inside a loop
+    - a loop pipe is either the start, or distance > 0
+    - if not a loop pipe : include me
+    - if my current loop count is odd, i'm in
+
+    No, that fails for the horizontal row of pipes, which is an odd number, so the far right cell thinks it's in.
+
+    I note that the real map has very few dots. Can I check those individually ?
+    Can I head out in all four directions, and the first one to the edge is the best indicator ?
+
+    I don't think I can use any left-hand/right-hand rules as my pipes head off in two directions.
+
+
+
+
 
      */
 
@@ -51,15 +68,22 @@ public class Year2023Day10 extends AdventOfCodeChallenge {
                 final String key = col + "," + row;
                 final Cell cell = this.cells.get(key);
                 if (cell.getContents().equalsIgnoreCase(".")) {
-                    line += ".";
+                    line += cell.getInsideOutside() == null ? "." : cell.getInsideOutside();
                     continue;
                 }
-                line += cell.getDistance() >= 0 ? cell.getDistance() : "?";
+                String draw = "?";
+                if (cell.getDistance() >= 0) {
+                    if (cell.getDistance() < 10) {
+                        draw = String.valueOf(cell.getDistance());
+                    } else {
+                        draw = "!";
+                    }
+                }
+                line += draw;
             }
             System.out.println(line);
         }
     }
-
 
     private int loadMap(final String[] input) {
 
@@ -208,7 +232,147 @@ public class Year2023Day10 extends AdventOfCodeChallenge {
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        this.loadMap(input);
+
+        final Cell testCell = this.getCell(5, 5);
+        this.insideByAllDirections(testCell);
+
+        final int inside = this.countInside4Ways();
+
+        this.drawMap();
+
+        return String.valueOf(inside);
+    }
+
+    private int countInside4Ways() {
+
+        int inside = 0;
+        for (int row = 0; row < this.height; row++) {
+            final int insideCount = 0;
+            for (int col = 0; col < this.width; col++) {
+                final Cell cell = this.getCell(col, row);
+                if (this.isJunk(cell)) {
+                    inside++;
+                    continue;
+                }
+                if (cell.getContents().equalsIgnoreCase(".")) {
+                    if (this.insideByAllDirections(cell)) {
+                        inside++;
+                    }
+                }
+            }
+        }
+
+        return inside;
+    }
+
+    private boolean insideByAllDirections(final Cell cell) {
+        int inside = 0;
+        inside = inside + this.trackSaysImInside(cell, "N");
+        inside = inside + this.trackSaysImInside(cell, "S");
+        inside = inside + this.trackSaysImInside(cell, "E");
+        inside = inside + this.trackSaysImInside(cell, "W");
+        if (inside == 0) {
+            return false;
+        }
+        if (inside == 4) {
+            return true;
+        }
+        throw new RuntimeException("ambiguous");
+    }
+
+    private int trackSaysImInside(final Cell cell, final String direction) {
+        int insideCount = 0;
+        Cell workingCell = cell;
+
+        while (true) {
+            workingCell = this.getNextCell(workingCell, direction);
+            if (workingCell == null) {
+                break;
+            }
+            final Cell nextWorkingCell = this.getNextCell(workingCell, direction);
+            if (this.isPipe(workingCell)) {
+                if (List.of("N", "S").contains(direction) && nextWorkingCell != null && this.hasEWExit(nextWorkingCell)) {
+                    continue;
+                }
+                if (List.of("E", "W").contains(direction) && nextWorkingCell != null && this.hasNSExit(nextWorkingCell)) {
+                    continue;
+                }
+                insideCount = insideCount + 1;
+            }
+        }
+
+        return insideCount % 2;
+    }
+
+    private boolean hasEWExit(final Cell workingCell) {
+        return workingCell.outE() || workingCell.outW();
+    }
+
+    private boolean hasNSExit(final Cell workingCell) {
+        return workingCell.outS() || workingCell.outN();
+    }
+
+    private Cell getNextCell(final Cell cell, final String direction) {
+        switch (direction.toUpperCase()) {
+            case "N":
+                return this.getCell(cell.getX(), cell.getY() - 1);
+            case "S":
+                return this.getCell(cell.getX(), cell.getY() + 1);
+            case "E":
+                return this.getCell(cell.getX() + 1, cell.getY());
+            case "W":
+                return this.getCell(cell.getX() - 1, cell.getY());
+            default:
+                throw new RuntimeException(direction);
+        }
+    }
+
+    private int countInsideFailed() {
+
+        // this not valid for solid pipes
+
+        int inside = 0;
+        for (int row = 0; row < this.height; row++) {
+            int insideCount = 0;
+            for (int col = 0; col < this.width; col++) {
+                final Cell cell = this.getCell(col, row);
+                if (this.isJunk(cell)) {
+                    inside++;
+                    continue;
+                }
+                if (this.isPipe(cell)) {
+                    insideCount++;
+                    continue;
+                }
+                if (cell.getContents().equalsIgnoreCase(".")) {
+                    if (insideCount % 2 == 1) {
+                        inside++;
+                        cell.setInsideOutside("I");
+                    } else {
+                        cell.setInsideOutside("O");
+                    }
+                    continue;
+                }
+                throw new RuntimeException("borked");
+            }
+        }
+        return inside;
+    }
+
+    private boolean isPipe(final Cell cell) {
+        return !cell.getContents().equalsIgnoreCase(".");
+    }
+
+    private boolean isJunk(final Cell cell) {
+        if (cell.isStart() || cell.getDistance() > 0) {
+            return false;
+        }
+        if (cell.getContents().equalsIgnoreCase(".")) {
+            return false;
+        }
+        return true;
     }
 
     @Data
@@ -218,6 +382,7 @@ public class Year2023Day10 extends AdventOfCodeChallenge {
         private int x;
         private int y;
         private String contents;
+        private String insideOutside;
         private int distance = -1;
         private boolean start = false;
 
--- src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
@@ -36,8 +36,8 @@ class Year2023DayTest {
     void part2() throws IOException {
 
         // given
-//        final Path path = Paths.get("src/main/resources/2023/sample.txt");
-        final Path path = Paths.get("src/main/resources/2023/2023-Day10-2.txt");
+        final Path path = Paths.get("src/main/resources/2023/sample.txt");
+//        final Path path = Paths.get("src/main/resources/2023/2023-Day10-2.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
         final Year2023Day10 Year2023Day10 = new Year2023Day10();
 
