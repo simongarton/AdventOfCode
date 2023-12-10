b8134f9b856471354cfccb0dad2b915779f4f49c
Simon Garton <simon.garton@gmail.com>
2023-12-10 18:55:36 +1300

    Day 10 part 1 done.

---
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day10.java
@@ -0,0 +1,241 @@
+package com.simongarton.adventofcode.year2023;
+
+import com.simongarton.adventofcode.AdventOfCodeChallenge;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Year2023Day10 extends AdventOfCodeChallenge {
+
+    /*
+
+    Start at S : that's 0.
+    For all connected neighbours, mark as N + 1;
+    For neighbours, I have to check if they ARE neighbours
+
+
+     */
+
+    private Map<String, Cell> cells;
+    private Cell start;
+    private int width;
+    private int height;
+
+    @Override
+    public String title() {
+        return "Day 10: ";
+    }
+
+    @Override
+    public Outcome run() {
+        return this.runChallenge(2023, 10);
+    }
+
+    @Override
+    public String part1(final String[] input) {
+
+        final int max = this.loadMap(input);
+
+        return String.valueOf(max);
+    }
+
+    private void drawMap() {
+
+        for (int row = 0; row < this.height; row++) {
+            String line = "";
+            for (int col = 0; col < this.width; col++) {
+                final String key = col + "," + row;
+                final Cell cell = this.cells.get(key);
+                if (cell.getContents().equalsIgnoreCase(".")) {
+                    line += ".";
+                    continue;
+                }
+                line += cell.getDistance() >= 0 ? cell.getDistance() : "?";
+            }
+            System.out.println(line);
+        }
+    }
+
+
+    private int loadMap(final String[] input) {
+
+        this.height = input.length;
+        this.width = input[0].length();
+
+        this.cells = new HashMap<>();
+        for (int row = 0; row < input.length; row++) {
+            final String line = input[row];
+            for (int col = 0; col < line.length(); col++) {
+                final String key = col + "," + row;
+                final Cell cell = this.figureOutCell(col, row, line.substring(col, col + 1));
+                this.cells.put(key, cell);
+                if (cell.start) {
+                    this.start = cell;
+                }
+            }
+        }
+
+        this.sortOutStart();
+        System.out.println("the start is a " + this.start.getContents());
+
+        this.drawMap();
+
+        final int max = this.sortOutDistances();
+        System.out.println();
+
+        this.drawMap();
+
+        return max;
+    }
+
+    private int sortOutDistances() {
+
+        int max = 0;
+
+        final List<Cell> cellsToCheck = new ArrayList<>();
+        cellsToCheck.add(this.start);
+
+        while (!cellsToCheck.isEmpty()) {
+            final Cell cellToCheck = cellsToCheck.get(0);
+            cellsToCheck.remove(0);
+            max = Math.max(max, cellToCheck.getDistance());
+            this.maybeAddNeighbour(cellToCheck, cellsToCheck, cellToCheck.getX() - 1, cellToCheck.getY());
+            this.maybeAddNeighbour(cellToCheck, cellsToCheck, cellToCheck.getX() + 1, cellToCheck.getY());
+            this.maybeAddNeighbour(cellToCheck, cellsToCheck, cellToCheck.getX(), cellToCheck.getY() - 1);
+            this.maybeAddNeighbour(cellToCheck, cellsToCheck, cellToCheck.getX(), cellToCheck.getY() + 1);
+
+//            this.drawMap();
+//            System.out.println();
+        }
+
+        return max;
+    }
+
+    private void maybeAddNeighbour(final Cell cellToCheck, final List<Cell> cellsToCheck, final int col, final int row) {
+        final Cell neighbour = this.getCell(col, row);
+        if (!this.canConnect(cellToCheck, neighbour)) {
+            // can't connect
+            return;
+        }
+        if (neighbour.getDistance() >= 0) {
+            // already connected
+            return;
+        }
+        neighbour.setDistance(cellToCheck.getDistance() + 1);
+        cellsToCheck.add(neighbour);
+    }
+
+    private boolean canConnect(final Cell cellToCheck, final Cell neighbour) {
+        if (cellToCheck == null || neighbour == null) {
+            return false;
+        }
+        if (cellToCheck.outN() && neighbour.outS() && cellToCheck.getX() == neighbour.getX() && cellToCheck.getY() > neighbour.getY()) {
+            return true;
+        }
+        if (cellToCheck.outS() && neighbour.outN() && cellToCheck.getX() == neighbour.getX() && cellToCheck.getY() < neighbour.getY()) {
+            return true;
+        }
+        if (cellToCheck.outW() && neighbour.outE() && cellToCheck.getY() == neighbour.getY() && cellToCheck.getX() > neighbour.getX()) {
+            return true;
+        }
+        if (cellToCheck.outE() && neighbour.outW() && cellToCheck.getY() == neighbour.getY() && cellToCheck.getX() < neighbour.getX()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void sortOutStart() {
+        this.start.setDistance(0);
+
+        // I'm NOT safe for edges
+        Cell cell = this.getCell(this.start.getX() - 1, this.start.getY());
+        final boolean needsW = cell != null && cell.outE();
+        cell = this.getCell(this.start.getX() + 1, this.start.getY());
+        final boolean needsE = cell != null && cell.outW();
+        cell = this.getCell(this.start.getX(), this.start.getY() - 1);
+        final boolean needsN = cell != null && cell.outS();
+        cell = this.getCell(this.start.getX(), this.start.getY() + 1);
+        final boolean needsS = cell != null && cell.outN();
+
+        if (needsN && needsE) {
+            this.start.setContents("L");
+            return;
+        }
+        if (needsN && needsS) {
+            this.start.setContents("|");
+            return;
+        }
+        if (needsN && needsW) {
+            this.start.setContents("J");
+            return;
+        }
+        if (needsS && needsW) {
+            this.start.setContents("7");
+            return;
+        }
+        if (needsS && needsE) {
+            this.start.setContents("F");
+            return;
+        }
+        if (needsW && needsE) {
+            this.start.setContents("-");
+            return;
+        }
+        throw new RuntimeException("broken");
+    }
+
+    private Cell getCell(final int col, final int row) {
+        final String key = col + "," + row;
+        if (!this.cells.containsKey(key)) {
+            return null;
+        }
+        return this.cells.get(key);
+    }
+
+    private Cell figureOutCell(final int col, final int row, final String substring) {
+        return Cell.builder()
+                .x(col)
+                .y(row)
+                .contents(substring)
+                .distance(-1)
+                .start(substring.equalsIgnoreCase("S"))
+                .build();
+    }
+
+    @Override
+    public String part2(final String[] input) {
+        return null;
+    }
+
+    @Data
+    @Builder
+    private static final class Cell {
+
+        private int x;
+        private int y;
+        private String contents;
+        private int distance = -1;
+        private boolean start = false;
+
+        public boolean outN() {
+            return List.of("|", "L", "J").contains(this.contents);
+        }
+
+        public boolean outE() {
+            return List.of("-", "L", "F").contains(this.contents);
+        }
+
+        public boolean outS() {
+            return List.of("|", "7", "F").contains(this.contents);
+        }
+
+        public boolean outW() {
+            return List.of("-", "J", "7").contains(this.contents);
+        }
+
+    }
+}
--- src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
+++ src/test/java/com/simongarton/adventofcode/year2023/Year2023DayTest.java
@@ -19,12 +19,12 @@ class Year2023DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2023/sample.txt");
-        final Path path = Paths.get("src/main/resources/2023/2023-Day9-1.txt");
+        final Path path = Paths.get("src/main/resources/2023/2023-Day10-1.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2023Day9 Year2023Day9 = new Year2023Day9();
+        final Year2023Day10 Year2023Day10 = new Year2023Day10();
 
         // when
-        final String result = Year2023Day9.part1(lines.toArray(new String[0]));
+        final String result = Year2023Day10.part1(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
@@ -37,12 +37,12 @@ class Year2023DayTest {
 
         // given
 //        final Path path = Paths.get("src/main/resources/2023/sample.txt");
-        final Path path = Paths.get("src/main/resources/2023/2023-Day9-2.txt");
+        final Path path = Paths.get("src/main/resources/2023/2023-Day10-2.txt");
         final List<String> lines = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.toList());
-        final Year2023Day9 Year2023Day9 = new Year2023Day9();
+        final Year2023Day10 Year2023Day10 = new Year2023Day10();
 
         // when
-        final String result = Year2023Day9.part2(lines.toArray(new String[0]));
+        final String result = Year2023Day10.part2(lines.toArray(new String[0]));
 
         // then
         assertNotNull(result);
