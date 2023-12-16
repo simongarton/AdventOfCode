2ac533461fd6eb1ad132452abcf488f9410be356
Simon Garton <simon.garton@gmail.com>
2023-12-16 20:50:01 +1300

    Looking hopeful.

--- src/python/2023-Day12-2.py
+++ src/python/2023-Day12-2.py
@@ -1,9 +1,11 @@
 def count(cfg, nums):
     print(cfg, nums)
     if cfg == '':
+        print("scoring cfg " + str(1 if nums == () else 0))
         return 1 if nums == () else 0
 
     if nums == ():
+        print("scoring nums " + str(0 if '#' in cfg else 1))
         return 0 if '#' in cfg else 1
 
     result = 0
@@ -25,4 +27,4 @@ for line in open('sample.txt'):
     nums = tuple(map(int, nums.split(',')))
     total += count(cfg, nums)
 
-print(total)
+# print(total)
