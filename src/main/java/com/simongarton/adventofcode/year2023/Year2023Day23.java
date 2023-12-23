091a57eb6e1129ee08b9d06d84ce55e9422d05cc
Simon Garton <simon.garton@gmail.com>
2023-12-23 22:28:37 +1300

    Finding 4 real intersections.

--- src/main/java/com/simongarton/adventofcode/year2023/Year2023Day23.java
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day23.java
@@ -11,12 +11,9 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
 
     private static final boolean DEBUG = true;
 
-    final Map<Integer, List<Tile>> openSet = new HashMap<>();
     private String map;
     private int width;
     private int height;
-    private Tile start;
-    private Tile end;
 
     private Map<Tile, Tile> cameFrom;
 
@@ -40,62 +37,110 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
         }
         this.cameFrom = new HashMap<>();
 
-        this.start = Tile.builder()
+        final Tile start = Tile.builder()
                 .x(1)
                 .y(0)
                 .cost(0)
                 .build();
-        this.end = Tile.builder()
+        final Tile end = Tile.builder()
                 .x(this.width - 2)
                 .y(this.height - 1)
                 .cost(0)
                 .build();
 
-        //this.aStar();
-        this.dfs();
-        final List<Tile> path = this.reconstructPath(this.end);
-        this.debugMap();
-
-        this.waitForKeys();
+        final Set<Tile> intersections = this.dfsToFindIntersections(start, end);
+        this.buildShortestPathMap(intersections);
+        this.drawCurrentMap(Collections.emptyList(), " ");
+        if (DEBUG) {
+            this.waitForKeys();
+        }
 
         // -1 as I have start AND end
-        return String.valueOf(path.size() - 1);
+        return String.valueOf(0);
     }
 
-    private void dfs() {
+    private Map<String, Integer> buildShortestPathMap(final Set<Tile> intersections) {
+        final List<Tile> intersectionList = new ArrayList<>(intersections);
+        final Map<String, Integer> distances = new HashMap<>();
+        for (final Tile from : intersectionList) {
+            for (final Tile to : intersectionList) {
+                if (from == to) {
+                    continue;
+                }
+                final String key = from.getAddress() + " -> " + to.getAddress();
+                System.out.println(key);
+                distances.put(key, this.aStar(from, to, intersections));
+            }
+        }
+        for (final Map.Entry<String, Integer> entry : distances.entrySet()) {
+            if (entry.getValue() != null) {
+                System.out.println(entry.getKey() + ":" + entry.getValue());
+            }
+        }
+        return distances;
+    }
+
+    private Set<Tile> dfsToFindIntersections(final Tile start, final Tile end) {
 
         final Set<Tile> visited = new HashSet<>();
         final Stack<Tile> stack = new Stack<>();
+        final Set<Tile> intersections = new HashSet<>();
 
-        stack.push(this.start);
-        visited.add(this.start);
+        stack.push(start);
+        visited.add(start);
 
         while (!stack.isEmpty()) {
             final Tile current = stack.pop();
-            for (final Tile neighbour : this.getNeighbours(current, visited)) {
+            final List<Tile> neighbours = this.getNeighbours(current, visited, end);
+            if (neighbours.size() > 1) {
+                final Optional<Tile> optionalIntersection = this.getIntersection(current, neighbours);
+                if (optionalIntersection.isPresent()) {
+                    System.out.println("Found intersection at " + current);
+                    visited.clear();
+                    visited.add(current);
+                    intersections.add(current);
+                    visited.addAll(intersections);
+                }
+            }
+            for (final Tile neighbour : neighbours) {
                 if (!visited.contains(neighbour)) {
                     visited.add(neighbour);
                     stack.push(neighbour);
                 }
             }
-            this.drawCurrentMap(visited);
+
+            this.drawCurrentMap(visited, "o");
         }
+        return intersections;
     }
 
-    private List<Tile> getNeighbours(final Tile tile, final Set<Tile> visited) {
+    private Optional<Tile> getIntersection(final Tile current, final List<Tile> neighbours) {
+        for (final Tile tile : neighbours) {
+            if (this.getFloor(tile.getX(), tile.getY()).equalsIgnoreCase(".")) {
+                return (Optional.empty());
+            }
+        }
+        return Optional.of(current);
+    }
+
+    private List<Tile> getNeighbours(final Tile tile, final Set<Tile> visited, final Tile end) {
         final List<Tile> neighbours = new ArrayList<>();
-        Optional<Tile> optionalTile = this.getNeighbour(tile, 1, 0, visited);
+        Optional<Tile> optionalTile = this.getNeighbour(tile, 1, 0, visited, end);
         optionalTile.ifPresent(neighbours::add);
-        optionalTile = this.getNeighbour(tile, -1, 0, visited);
+        optionalTile = this.getNeighbour(tile, -1, 0, visited, end);
         optionalTile.ifPresent(neighbours::add);
-        optionalTile = this.getNeighbour(tile, 0, 1, visited);
+        optionalTile = this.getNeighbour(tile, 0, 1, visited, end);
         optionalTile.ifPresent(neighbours::add);
-        optionalTile = this.getNeighbour(tile, 0, -1, visited);
+        optionalTile = this.getNeighbour(tile, 0, -1, visited, end);
         optionalTile.ifPresent(neighbours::add);
         return neighbours;
     }
 
-    private Optional<Tile> getNeighbour(final Tile tile, final int deltaX, final int deltaY, final Set<Tile> visited) {
+    private Optional<Tile> getNeighbour(final Tile tile,
+                                        final int deltaX,
+                                        final int deltaY,
+                                        final Set<Tile> visited,
+                                        final Tile end) {
         final int x = tile.getX() + deltaX;
         final int y = tile.getY() + deltaY;
         final int cost = tile.getCost() + 1;
@@ -127,8 +172,7 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
                 .cost(cost)
                 .build();
 
-        if (newTile.getX() == this.end.getX() && newTile.getY() == this.end.getY()) {
-            System.out.println("Found end with cost " + cost);
+        if (newTile.getX() == end.getX() && newTile.getY() == end.getY()) {
             return Optional.of(newTile);
         }
 
@@ -139,29 +183,65 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
         return Optional.of(newTile);
     }
 
-    private void aStar() {
+    private Integer aStar(final Tile start, final Tile end, final Set<Tile> intersections) {
 
-        this.openSet.clear();
-        this.openSet.put(0, List.of(this.start));
+        final Map<Integer, List<Tile>> openSet = new HashMap<>();
+        final Set<Tile> visited = new HashSet<>();
+        visited.add(start);
+        this.cameFrom.clear();
+        openSet.put(0, List.of(start));
 
-        while (!this.openSet.isEmpty()) {
-            final int minCost = this.openSet.keySet().stream().min(Integer::compareTo).get();
-            final List<Tile> tiles = this.openSet.remove(minCost);
+        // not sure this is right, but we are getting stuck going from 3,5 -> 11,3
+//        for (final Tile tile : intersections) {
+//            if (tile == start || tile == end) {
+//                continue;
+//            }
+//            visited.add(tile);
+//        }
+
+        while (!openSet.isEmpty()) {
+            final int minCost = openSet.keySet().stream().min(Integer::compareTo).get();
+            final List<Tile> tiles = openSet.remove(minCost);
             for (final Tile tile : tiles) {
-                this.neighbours(tile);
+                final List<Tile> neighbours = this.neighbours(tile, end, openSet, visited);
+                final Integer distance = this.findEnd(neighbours, end);
+                if (distance != null) {
+                    if (DEBUG) {
+                        final List<Tile> path = this.reconstructPath(end);
+                        this.drawCurrentMap(intersections, "!");
+                        this.sleep(1000);
+                        this.drawCurrentMap(path, "x");
+                        this.sleep(1000);
+                    }
+                    return distance;
+                }
             }
-            if (DEBUG) {
-                final List<Tile> path = this.reconstructPath(this.end);
-                this.drawCurrentMap(path);
-            }
+        }
+        return null;
+    }
+
+    private void sleep(final int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void drawCurrentMap(final Collection<Tile> tiles) {
+    private Integer findEnd(final List<Tile> neighbours, final Tile end) {
+        for (final Tile neighbour : neighbours) {
+            if (neighbour.getX() == end.getX() && neighbour.getY() == end.getY()) {
+                return neighbour.getCost();
+            }
+        }
+        return null;
+    }
+
+    private void drawCurrentMap(final Collection<Tile> tiles, final String tileSymbol) {
 
         String newMap = this.map;
         for (final Tile tile : tiles) {
-            newMap = this.replaceCharacter(newMap, tile.getX(), tile.getY(), this.width, "o");
+            newMap = this.replaceCharacter(newMap, tile.getX(), tile.getY(), this.width, tileSymbol);
         }
 
         for (int i = 0; i < this.height; i++) {
@@ -186,6 +266,10 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
                 return TextColor.ANSI.GREEN;
             case 'o':
                 return TextColor.ANSI.RED_BRIGHT;
+            case 'x':
+                return TextColor.ANSI.BLUE_BRIGHT;
+            case '!':
+                return TextColor.ANSI.GREEN_BRIGHT;
             default:
                 return TextColor.ANSI.WHITE;
         }
@@ -199,55 +283,79 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
         this.blankLine();
     }
 
-    private void neighbours(final Tile tile) {
-        this.neighbour(tile, 1, 0);
-        this.neighbour(tile, -1, 0);
-        this.neighbour(tile, 0, 1);
-        this.neighbour(tile, 0, -1);
+    private List<Tile> neighbours(final Tile tile, final Tile end, final Map<Integer, List<Tile>> openSet, final Set<Tile> visited) {
+        final List<Tile> neighbours = new ArrayList<>();
+        Optional<Tile> optionalTile = this.neighbour(tile, 1, 0, end, openSet, visited);
+        optionalTile.ifPresent(neighbours::add);
+        optionalTile = this.neighbour(tile, -1, 0, end, openSet, visited);
+        optionalTile.ifPresent(neighbours::add);
+        optionalTile = this.neighbour(tile, 0, 1, end, openSet, visited);
+        optionalTile.ifPresent(neighbours::add);
+        optionalTile = this.neighbour(tile, 0, -1, end, openSet, visited);
+        optionalTile.ifPresent(neighbours::add);
+        return neighbours;
     }
 
-    private void neighbour(final Tile tile, final int deltaX, final int deltaY) {
+    private Optional<Tile> neighbour(final Tile tile,
+                                     final int deltaX,
+                                     final int deltaY,
+                                     final Tile end,
+                                     final Map<Integer, List<Tile>> openSet,
+                                     final Set<Tile> visited) {
+
+        final Tile newTile = this.getNewTile(tile, deltaX, deltaY, end);
+        if (visited.contains(newTile)) {
+            return Optional.empty();
+        }
+        if (newTile == null) {
+            return Optional.empty();
+        }
+
+        if (newTile.getX() == end.getX() && newTile.getY() == end.getY()) {
+            this.cameFrom.put(end, tile);
+            return Optional.of(newTile);
+        }
+
+        final List<Tile> tiles = openSet.getOrDefault(newTile.getCost(), new ArrayList<>());
+        tiles.add(newTile);
+        visited.add(newTile);
+        this.cameFrom.put(newTile, tile);
+        openSet.put(newTile.getCost(), tiles);
+        return Optional.of(newTile);
+    }
+
+    private Tile getNewTile(final Tile tile, final int deltaX, final int deltaY, final Tile end) {
+
         final int x = tile.getX() + deltaX;
         final int y = tile.getY() + deltaY;
         final int cost = tile.getCost() + 1;
 
         if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
-            return;
+            return null;
         }
         final String floor = this.getFloor(x, y);
         if (floor.equalsIgnoreCase("#") || floor.equalsIgnoreCase(" ")) {
-            return;
+            return null;
         }
 
         if (floor.equalsIgnoreCase(">") && deltaX != 1) {
-            return;
+            return null;
         }
         if (floor.equalsIgnoreCase("<") && deltaX != -1) {
-            return;
+            return null;
         }
         if (floor.equalsIgnoreCase("v") && deltaY != 1) {
-            return;
+            return null;
         }
         if (floor.equalsIgnoreCase("^") && deltaY != -1) {
-            return;
+            return null;
         }
 
-        final Tile newTile = Tile.builder()
+        return Tile.builder()
                 .x(x)
                 .y(y)
                 .cost(cost)
                 .build();
-
-        if (newTile.getX() == this.end.getX() && newTile.getY() == this.end.getY()) {
-            this.cameFrom.put(this.end, tile);
-            return;
-        }
-
-        final List<Tile> tiles = this.openSet.getOrDefault(cost, new ArrayList<>());
-        tiles.add(newTile);
-        this.map = this.replaceCharacter(this.map, x, y, this.width, " ");
-        this.cameFrom.put(newTile, tile);
-        this.openSet.put(cost, tiles);
     }
 
     private String getFloor(final int x, final int y) {
@@ -256,25 +364,24 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
         return this.map.substring(index, index + 1);
     }
 
-    private Integer h(final Tile tile) {
-        return this.manhattanDistance(tile, this.end);
-    }
-
-    private int manhattanDistance(final Tile from, final Tile to) {
-        return Math.abs(from.getX() - to.getX()) + Math.abs(from.getY() - to.getY());
-    }
-
     private List<Tile> reconstructPath(final Tile end) {
 
+        final Set<Tile> visited = new HashSet<>();
         final List<Tile> path = new ArrayList<>();
         Tile current = end;
         while (true) {
             path.add(0, current);
-            this.map = this.replaceCharacter(this.map, current.getX(), current.getY(), this.width, "o");
             if (!this.cameFrom.containsKey(current)) {
                 break;
             }
             current = this.cameFrom.get(current);
+            if (visited.contains(current)) {
+                for (final Map.Entry<Tile, Tile> entry : this.cameFrom.entrySet()) {
+                    System.out.println(entry.getKey().getAddress() + " : " + entry.getValue().getAddress());
+                }
+                throw new RuntimeException("Inception");
+            }
+            visited.add(current);
         }
 
         return path;
@@ -315,6 +422,10 @@ public class Year2023Day23 extends AdventOfCodeChallenge {
         @Override
         public int hashCode() {
             return Objects.hash(this.x, this.y);
+        }
+
+        public String getAddress() {
+            return this.x + "," + this.y;
         }
     }
 }
