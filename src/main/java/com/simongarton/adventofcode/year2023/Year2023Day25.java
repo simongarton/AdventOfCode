5498598407adf4466901e57b963037fa5fe20fd8
Simon Garton <simon.garton@gmail.com>
2023-12-26 16:35:57 +1300

    2023 Day 25 part 1 early ideas.

---
+++ src/main/java/com/simongarton/adventofcode/year2023/Year2023Day25.java
@@ -0,0 +1,26 @@
+package com.simongarton.adventofcode.year2023;
+
+import com.simongarton.adventofcode.AdventOfCodeChallenge;
+
+public class Year2023Day24 extends AdventOfCodeChallenge {
+
+    @Override
+    public String title() {
+        return "Day 24: Template code";
+    }
+
+    @Override
+    public Outcome run() {
+        return this.runChallenge(2023, 24);
+    }
+
+    @Override
+    public String part1(final String[] input) {
+        return null;
+    }
+
+    @Override
+    public String part2(final String[] input) {
+        return null;
+    }
+}
