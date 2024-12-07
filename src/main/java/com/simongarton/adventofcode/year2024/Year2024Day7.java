d9156bd6184214a57d56b840220e189888c50e3b
Simon Garton <simon.garton@gmail.com>
2024-12-07 19:27:44 +1300

    misunderstood instructions, reworking.

--- src/main/java/com/simongarton/adventofcode/year2024/Year2024Day7.java
+++ src/main/java/com/simongarton/adventofcode/year2024/Year2024Day7.java
@@ -33,11 +33,52 @@ public class Year2024Day7 extends AdventOfCodeChallenge {
                 score += equation.answer;
             }
         }
+
         return String.valueOf(score);
+    }
+
+    private boolean evaluateEquationVariations(final Equation equation,
+                                               final List<String> operatorTypes) {
+
+        // I need to build new equations : I need to get the original one, plus then I need to
+        // join all the possible pairs of numbers.
+        final List<Equation> equations = new ArrayList<>();
+        equations.add(equation);
+
+        for (int i = 1; i < equation.numbers.size(); i++) {
+            final Equation newEquation = new Equation(equation.line);
+            newEquation.answer = equation.answer;
+            for (int n = 0; n < equation.numbers.size(); n++) {
+                if (n == i) {
+                    continue;
+                }
+                if (n == (i - 1)) {
+                    final int newValue = Integer.parseInt(
+                            (equation.numbers.get(n) + "") +
+                                    (equation.numbers.get(n + 1) + "")
+                    );
+                    newEquation.numbers.add(newValue);
+                    continue;
+                }
+                newEquation.numbers.add(equation.numbers.get(n));
+            }
+            equations.add(newEquation);
+        }
+
+        for (final Equation equation1 : equations) {
+            final int operatorLength = equation1.numbers.size() - 1;
+            final List<String> operatorCombinations = this.generateCombinations(operatorTypes, operatorLength);
+            if (this.evaluateEquation(equation1, operatorCombinations)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private boolean evaluateEquation(final Equation equation, final List<String> operatorCombinations) {
 
+        System.out.println("evaluating " + equation + " " + equation.numbers);
         for (final String combination : operatorCombinations) {
             if (this.evaluateEquationWithCombination(equation, combination)) {
                 return true;
@@ -49,6 +90,10 @@ public class Year2024Day7 extends AdventOfCodeChallenge {
     private boolean evaluateEquationWithCombination(final Equation equation, final String combination) {
 
         long total = 0;
+        if (equation.numbers.size() == 1) {
+            total = equation.numbers.get(0);
+            return total == equation.answer;
+        }
         for (int index = 1; index < equation.numbers.size(); index++) {
             if (index == 1) {
                 total = this.applyOperator(equation.numbers.get(0),
@@ -128,28 +173,47 @@ public class Year2024Day7 extends AdventOfCodeChallenge {
 
     private Equation startEquation(final String line) {
 
-        final Equation equation = new Equation();
+        final Equation equation = new Equation(line);
         final String[] parts = line.split(":");
         equation.answer = Long.parseLong(parts[0]);
         final String[] operands = parts[1].trim().split(" ");
-        equation.numbers.addAll(Arrays.asList(operands).stream().map(Integer::parseInt).collect(Collectors.toList()));
+        equation.numbers.addAll(Arrays.stream(operands).map(Integer::parseInt).collect(Collectors.toList()));
         return equation;
     }
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        final List<Equation> equations = this.parseEquations(input);
+
+        long score = 0;
+        final List<String> operatorTypes = List.of("*", "+");
+        for (final Equation equation : equations) {
+            if (this.evaluateEquationVariations(equation, operatorTypes)) {
+                System.out.println(equation + " worked so adding " + equation.answer);
+                score += equation.answer;
+            } else {
+                System.out.println(equation + " failed");
+            }
+        }
+
+        return String.valueOf(score);
     }
 
     static class Equation {
 
+        String line;
         Long answer;
         List<Integer> numbers;
-        List<String> operators;
 
-        public Equation() {
+        public Equation(final String line) {
+            this.line = line;
             this.numbers = new ArrayList<>();
-            this.operators = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            return this.line + " = " + this.numbers;
         }
     }
 }
