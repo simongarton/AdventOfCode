44bf475e2d36d7477d3eb2cadf096e630a3c6c07
Simon Garton <simon.garton@gmail.com>
2024-12-08 20:32:23 +1300

    Cleanup part 1.

--- src/main/java/com/simongarton/adventofcode/year2024/Year2024Day8.java
+++ src/main/java/com/simongarton/adventofcode/year2024/Year2024Day8.java
@@ -25,13 +25,8 @@ public class Year2024Day8 extends AdventOfCodeChallenge {
     @Override
     public String part1(final String[] input) {
 
-        this.loadChallengeMap(input);
-        this.drawChallengeMap();
-        this.loadAntennas();
-        final List<String> frequencies = this.findFrequencies();
+        final List<String> frequencies = this.setup(input);
 
-        this.antiNodes = new ArrayList<>();
-        this.antiNodeMap = new HashMap<>();
         for (final String frequency : frequencies) {
             this.addAntiNodesForFrequency(frequency);
         }
@@ -45,96 +40,85 @@ public class Year2024Day8 extends AdventOfCodeChallenge {
         return String.valueOf(this.antiNodes.size());
     }
 
+    private List<String> setup(final String[] input) {
+        this.loadChallengeMap(input);
+        this.drawChallengeMap();
+        this.loadAntennas();
+        final List<String> frequencies = this.findFrequencies();
+
+        this.antiNodes = new ArrayList<>();
+        this.antiNodeMap = new HashMap<>();
+
+        return frequencies;
+    }
+
     private void addAntiNodesForFrequency(final String frequency) {
 
-        System.out.println("Looking for antinodes for frequency " + frequency);
         final List<Antenna> antennaList = this.antennas.stream()
                 .filter(a -> a.frequency.equals(frequency)) // not case sensitive !
                 .collect(Collectors.toList());
 
-        System.out.println(" I have " + antennaList.size() + " antennas to check.");
         for (final Antenna antenna : antennaList) {
             System.out.println("  " + antenna);
         }
 
         for (final Antenna antenna1 : antennaList) {
             for (final Antenna antenna2 : antennaList) {
-                System.out.println(" looking " + antenna1 + " with " + antenna2);
                 if (antenna1.key().equalsIgnoreCase(antenna2.key())) {
                     continue;
                 }
-                System.out.println(" comparing " + antenna1 + " with " + antenna2);
                 final int deltaX = antenna2.x - antenna1.x;
                 final int deltaY = antenna2.y - antenna1.y;
                 final int x = antenna2.x + deltaX;
                 final int y = antenna2.y + deltaY;
-                System.out.println("  looking at " + this.getCoordKey(x, y));
 
                 if (this.getChallengeMapLetter(x, y) == null) {
-                    System.out.println("  off map");
                     continue;
                 }
 
-                final String coordKey = this.getCoordKey(x, y);
-                if (this.getAntiNodeForXY(x, y).isPresent()) {
-                    System.out.println("  node there");
-                    continue;
-                }
-                final AntiNode antiNode = new AntiNode(x, y, frequency);
-                this.antiNodes.add(antiNode);
-                this.antiNodeMap.put(coordKey, antiNode);
-                System.out.println("  creating antinode " + antiNode);
+                this.createAntiNodeIfNeeded(x, y, frequency);
             }
         }
     }
 
+    private void createAntiNodeIfNeeded(final int x, final int y, final String frequency) {
+
+        final String coordKey = this.getCoordKey(x, y);
+        if (this.getAntiNodeForXY(x, y).isEmpty()) {
+            final AntiNode antiNode = new AntiNode(x, y, frequency);
+            this.antiNodes.add(antiNode);
+            this.antiNodeMap.put(coordKey, antiNode);
+        }
+    }
+
     private String getCoordKey(final int x, final int y) {
+
         return x + "," + y;
     }
 
     private void addAntiNodesForResonantFrequency(final String frequency) {
 
-        System.out.println("Looking for antinodes for frequency " + frequency);
         final List<Antenna> antennaList = this.antennas.stream()
                 .filter(a -> a.frequency.equals(frequency)) // not case sensitive !
                 .collect(Collectors.toList());
 
-        System.out.println(" I have " + antennaList.size() + " antennas to check.");
-        for (final Antenna antenna : antennaList) {
-            System.out.println("  " + antenna);
-        }
-
         for (final Antenna antenna1 : antennaList) {
             for (final Antenna antenna2 : antennaList) {
-                System.out.println(" looking " + antenna1 + " with " + antenna2);
                 if (antenna1.key().equalsIgnoreCase(antenna2.key())) {
                     continue;
                 }
-                System.out.println(" comparing " + antenna1 + " with " + antenna2);
                 final int deltaX = antenna2.x - antenna1.x;
                 final int deltaY = antenna2.y - antenna1.y;
                 int x = antenna2.x;
                 int y = antenna2.y;
 
                 while (true) {
-                    System.out.println("  looking at " + this.getCoordKey(x, y));
-
                     if (this.getChallengeMapLetter(x, y) == null) {
                         System.out.println("  off map");
                         break;
                     }
 
-                    final String coordKey = this.getCoordKey(x, y);
-                    if (this.getAntiNodeForXY(x, y).isPresent()) {
-                        System.out.println("  node there");
-                        x = x + deltaX;
-                        y = y + deltaY;
-                        continue;
-                    }
-                    final AntiNode antiNode = new AntiNode(x, y, frequency);
-                    this.antiNodes.add(antiNode);
-                    this.antiNodeMap.put(coordKey, antiNode);
-                    System.out.println("  creating antinode " + antiNode);
+                    this.createAntiNodeIfNeeded(x, y, frequency);
 
                     x = x + deltaX;
                     y = y + deltaY;
@@ -153,33 +137,13 @@ public class Year2024Day8 extends AdventOfCodeChallenge {
         return Optional.empty();
     }
 
-    private Optional<Antenna> getAntennaForXY(final int x, final int y, final String frequency) {
-
-        final String coordKey = this.getCoordKey(x, y);
-        if (this.antennaMap.containsKey(coordKey)) {
-            final Antenna antenna = this.antennaMap.get(coordKey);
-            // it should never NOT match
-            if (antenna.frequency.equals(frequency)) {
-                return Optional.of(antenna);
-            }
-        }
-        return Optional.empty();
-    }
-
-    private Optional<Antenna> getAntennaForXY(final int x, final int y) {
-
-        final String coordKey = this.getCoordKey(x, y);
-        if (this.antennaMap.containsKey(coordKey)) {
-            return Optional.of(this.antennaMap.get(coordKey));
-        }
-        return Optional.empty();
-    }
-
     private List<String> findFrequencies() {
 
-        final Set<String> frequencies = this.antennas.stream().map(a -> a.frequency).collect(Collectors.toSet());
-        final List<String> frequencyList = new ArrayList<>(frequencies);
-        frequencyList.sort(Comparator.naturalOrder());
+        final List<String> frequencyList = this.antennas.stream()
+                .map(a -> a.frequency)
+                .distinct()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
         return frequencyList;
     }
 
@@ -215,19 +179,13 @@ public class Year2024Day8 extends AdventOfCodeChallenge {
         final String coordKey = this.getCoordKey(x, y);
         this.antennaMap.put(coordKey, antenna);
         return antenna;
-
     }
 
     @Override
     public String part2(final String[] input) {
 
-        this.loadChallengeMap(input);
-        this.drawChallengeMap();
-        this.loadAntennas();
-        final List<String> frequencies = this.findFrequencies();
+        final List<String> frequencies = this.setup(input);
 
-        this.antiNodes = new ArrayList<>();
-        this.antiNodeMap = new HashMap<>();
         for (final String frequency : frequencies) {
             this.addAntiNodesForResonantFrequency(frequency);
         }
