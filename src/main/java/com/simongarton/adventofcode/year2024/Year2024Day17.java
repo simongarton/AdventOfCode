f83fcd7a701d555743057071e6afa386a2fefa44
Simon Garton <simon.garton@gmail.com>
2024-12-18 21:51:57 +1300

    Looks like longs are back on the menu, boys.

--- src/main/java/com/simongarton/adventofcode/year2024/Year2024Day17.java
+++ src/main/java/com/simongarton/adventofcode/year2024/Year2024Day17.java
@@ -2,7 +2,12 @@ package com.simongarton.adventofcode.year2024;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
 
+import java.util.List;
+
 public class Year2024Day17 extends AdventOfCodeChallenge {
+
+    private final List<Integer> part2Program = List.of(2, 4, 1, 1, 7, 5, 4, 4, 1, 4, 0, 3, 5, 5, 3, 0);
+    private int bestDepthFound;
 
     @Override
     public String title() {
@@ -30,11 +35,15 @@ public class Year2024Day17 extends AdventOfCodeChallenge {
         final ChronospatialComputer computer = ChronospatialComputer.initializeFromLines(input);
         int index = 1;
         int validA = 0;
-        int checked = 0;
+        this.bestDepthFound = 0;
+        final int checked = 0;
         while (true) {
-            final Integer a = this.evaluatePossibleA(index);
+            final Integer a = this.evaluatePossibleAForAll(index);
+            index++;
+            if (index % 10000000 == 0) {
+                System.out.println(index);
+            }
             if (a == null) {
-                index++;
                 continue;
             }
             computer.reset();
@@ -44,10 +53,9 @@ public class Year2024Day17 extends AdventOfCodeChallenge {
                 validA = a;
                 break;
             }
-            if (++checked % 10000 == 0) {
-                System.out.println(checked + " -> " + a + " p " + computer.getProgramString() + " -> " + computer.getOutputString());
-            }
-            index++;
+//            if (++checked % 10000 == 0) {
+            System.out.println(checked + " -> " + a + " p " + computer.getProgramString() + " -> " + computer.getOutputString());
+//            }
         }
 
         return String.valueOf(validA);
@@ -65,9 +73,37 @@ public class Year2024Day17 extends AdventOfCodeChallenge {
         b = b ^ c;
         b = b ^ 4;
         a = a / 8;
-        if (b % 8 == 2) {
-            return index;
+        if (b % 8 != 2) {
+            return null;
         }
         return null;
+    }
+
+    private Integer evaluatePossibleAForAll(final int index) {
+
+        int a = index;
+        int b = 0;
+        int c = 0;
+
+        for (int iteration = 0; iteration < this.part2Program.size(); iteration++) {
+            b = a % 8;
+            b = b ^ 1;
+            c = Double.valueOf(Math.floor(1.0D * a / Math.pow(2, b))).intValue();
+            b = b ^ c;
+            b = b ^ 4;
+            a = a / 8;
+            if (b % 8 != this.part2Program.get(iteration)) {
+                if (iteration > this.bestDepthFound) {
+                    this.bestDepthFound = iteration;
+                    String best = "";
+                    for (int i = 0; i < this.bestDepthFound; i++) {
+                        best = best + this.part2Program.get(i) + ",";
+                    }
+                    System.out.println("bestDepthFound " + this.bestDepthFound + "/" + this.part2Program.size() + " = " + best);
+                }
+                return null;
+            }
+        }
+        return index;
     }
 }
