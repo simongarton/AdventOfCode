8d29c83a8d6b17bb156b40399e19504183f82158
Simon Garton <simon.garton@gmail.com>
2025-01-13 13:40:50 +1300

    2015-4 done.

--- src/main/java/com/simongarton/adventofcode/CurrentChallenge.java
+++ src/main/java/com/simongarton/adventofcode/CurrentChallenge.java
@@ -1,14 +1,14 @@
 package com.simongarton.adventofcode;
 
-import com.simongarton.adventofcode.year2015.Year2015Day3;
+import com.simongarton.adventofcode.year2015.Year2015Day4;
 
 public class CurrentChallenge {
 
-    public static String DAY = "3";
+    public static String DAY = "4";
     public static int YEAR = 2015;
 
     public static AdventOfCodeChallenge getCurrentChallenge() {
 
-        return new Year2015Day3();
+        return new Year2015Day4();
     }
 }
--- src/main/java/com/simongarton/adventofcode/year2015/Year2015Day4.java
+++ src/main/java/com/simongarton/adventofcode/year2015/Year2015Day4.java
@@ -2,11 +2,15 @@ package com.simongarton.adventofcode.year2015;
 
 import com.simongarton.adventofcode.AdventOfCodeChallenge;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Year2015Day4 extends AdventOfCodeChallenge {
 
     @Override
     public String title() {
-        return "Day 0: Template code";
+        return "Day 4: The Ideal Stocking Stuffer";
     }
 
     @Override
@@ -16,11 +20,43 @@ public class Year2015Day4 extends AdventOfCodeChallenge {
 
     @Override
     public String part1(final String[] input) {
-        return null;
+
+        int i = 0;
+        while (true) {
+            final String output = this.hash(input[0] + i);
+            if (output.substring(0, 5).equalsIgnoreCase("00000")) {
+                break;
+            }
+            i++;
+        }
+
+        return String.valueOf(i);
+    }
+
+    private String hash(final String password) {
+
+        try {
+            final MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            final byte[] digest = md.digest();
+            return DatatypeConverter.printHexBinary(digest).toUpperCase();
+        } catch (final NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String part2(final String[] input) {
-        return null;
+
+        int i = 0;
+        while (true) {
+            final String output = this.hash(input[0] + i);
+            if (output.substring(0, 6).equalsIgnoreCase("000000")) {
+                break;
+            }
+            i++;
+        }
+
+        return String.valueOf(i);
     }
 }
